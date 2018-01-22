714d656dbdf224d358b7f7a3cc66139254f97872
Jim Zhang <jim.zoumo@gmail.com>
2018-01-22 18:16:40 +0800

    feat(scripts): add shell scripts implementation for make rules

---
+++ hack/make-rules/entrypoint/golang.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in pipeline.
+set -o pipefail
+
+MAKR_RULES_ROOT=$(dirname "${BASH_SOURCE}")/..
+VERBOSE="${VERBOSE:-1}"
+source "${MAKR_RULES_ROOT}/lib/init.sh"
+
+build() {
+	golang::build_binaries "$@"
+	golang::place_bins
+}
+
+unittest() {
+	golang::unittest "$@"
+}
+
+usage() {
+	log::usage_from_stdin <<EOF
+usage: $cmd <commands> [TARGETS]
+
+Available Commands:
+    build      build go package
+    unittest   test all package in this project
+EOF
+}
+
+subcommand=${1-}
+case $subcommand in
+	"" | "-h" | "--help")
+		usage
+		;;
+	*)
+		shift
+		${subcommand} $@
+		;;
+esac
